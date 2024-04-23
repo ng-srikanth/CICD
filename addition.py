c5c2fec8958f8478a5bf17617e26b27af27eaae9
ng-srikanth <srikanth.b@ngenux.com>
2024-04-23 16:34:54 +0530

    creating coverage issues

--- addition.py
+++ addition.py
@@ -1,27 +1,6 @@
-# def add(x, y):
-#     return x + y
-# def sub(x,y):
-#     return x-y
-# add(1,2)
-# sub(2,1)
 def add(x, y):
-    if x > 0:
-        return x + y
-    else:
-        return y
-
-def sub(x, y):
-    return x - y
-
-def create_duplicate(x, y):
-    if x > 0:
-        return x + y
-    else:
-        return y
-def create_duplicate_sub(x,y):
-    return x - y
-
-add(1, 2)
-sub(2, 1)
-create_duplicate(1, 3)
-create_duplicate_sub(6,3)
+    return x + y
+def sub(x,y):
+    return x-y
+add(1,2)
+sub(2,1)
