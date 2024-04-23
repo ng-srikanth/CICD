23f954f759116e36519ca213e24e0a668d832901
ng-srikanth <srikanth.b@ngenux.com>
2024-04-23 16:04:17 +0530

    updated test cases

--- addition.py
+++ addition.py
@@ -1,6 +1,17 @@
+# def add(x, y):
+#     return x + y
+# def sub(x,y):
+#     return x-y
+# add(1,2)
+# sub(2,1)
 def add(x, y):
-    return x + y
-def sub(x,y):
-    return x-y
-add(1,2)
-sub(2,1)
+    if x > 0:
+        return x + y
+    else:
+        return y
+
+def sub(x, y):
+    return x - y
+
+add(1, 2)
+sub(2, 1)
--- test_module.py
+++ test_module.py
@@ -3,4 +3,4 @@ from addition import add,sub
 def test_add():
     assert add(3, 5) == 8
 def test_sub():
-    assert sub(3,5) == -8
+    assert sub(3,5) == -2
