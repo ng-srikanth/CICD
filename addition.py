c36f43f578568ba261ce3519ee4c11a6e9e80ac6
ng-srikanth <srikanth.b@ngenux.com>
2024-04-23 17:44:06 +0530

    updated addition.py

--- addition.py
+++ addition.py
@@ -6,12 +6,12 @@
 # sub(2,1)
 def add(x, y):
     if x > 0:
-        return x + y
+        return x +y
     else:
         return y
 
 def sub(x, y):
-    return x -y
+    return x - y
 
 add(1, 2)
 sub(2, 1)
