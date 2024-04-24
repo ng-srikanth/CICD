5b8792baf69126407ebd9cc1937c21a57f90438a
ng-srikanth <srikanth.b@ngenux.com>
2024-04-24 10:42:43 +0530

    testing covrage

--- addition.py
+++ addition.py
@@ -1,14 +1,9 @@
-# def add(x, y):
-#     return x + y
-# def sub(x,y):
-#     return x-y
-# add(1,2)
-# sub(2,1)
 def add(x, y):
-    if x > 0:
-        return x + y
-    else:
-        return y
+    # if x > 0:
+    #     return x + y
+    # else:
+    #     return y
+    return x + y
 
 def sub(x, y):
     return x - y
