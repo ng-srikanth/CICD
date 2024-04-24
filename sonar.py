dce84923b5dfe39da7589317dc9d2e55a5588b0f
ng-srikanth <srikanth.b@ngenux.com>
2024-04-24 18:26:35 +0530

    test pull requests

--- sonar.py
+++ sonar.py
@@ -3,7 +3,7 @@ def count_even_odd(n):
     count_odd = 0
     for i in range(n):
         if i%2 == 0:
-            count_even+=1
+            count_even += 1
         else:
             count_odd+=1
     return count_even,count_odd
