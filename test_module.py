6899c74668b5e804f68c0d2c7abfbcca881e6689
ng-srikanth <srikanth.b@ngenux.com>
2024-04-24 12:46:52 +0530

    testing coverage with conditions

--- test_module.py
+++ test_module.py
@@ -1,6 +1,10 @@
-from addition import add,sub
+from addition import add,sub,count_even_odd
 
 def test_add():
     assert add(3, 5) == 8
 def test_sub():
     assert sub(3,5) == -2
+
+def test_count_even_odd():
+
+    assert count_even_odd(101) == (51, 50), "Test case failed"
