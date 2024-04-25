6edf41cf05d6ad9757190f4cf9091c72b0f911b7
ng-srikanth <srikanth.b@ngenux.com>
2024-04-25 10:24:29 +0530

    checking coverage value

--- sonartest.py
+++ sonartest.py
@@ -4,8 +4,10 @@ def count_even_odd(n):
     for i in range(n):
         if i%2 == 0:
             count_even += 1
-        else:
+        elif i%2 != 0:
             count_odd+=1
+        else:
+            pass
     return count_even,count_odd
 
 count_even_odd(100)
